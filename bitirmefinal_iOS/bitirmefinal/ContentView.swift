b41c831a03cbec12e1aafacf58e670a68bc8f3d9
Atakan Apan <atakanapan12@gmail.com>
2025-02-21 14:53:09 +0000

    Merged in feature/BF-4-Create-ios-project (pull request #1)
    Create initial ios project
    
    * Create initial ios project
    
    Create initial ios project
    
    
    Approved-by: Yalçın Yağız Korkmaz

---
+++ bitirmefinal_iOS/bitirmefinal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bitirmefinal
+//
+//  Created by Atakan Apan on 20.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bitirmefinal_iOS/bitirmefinal/bitirmefinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  bitirmefinalApp.swift
+//  bitirmefinal
+//
+//  Created by Atakan Apan on 20.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct bitirmefinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
